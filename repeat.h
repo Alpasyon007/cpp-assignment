fa51ac1b0db104c8f85cd6133bce16ba8a56e902
martabarbera <82161466+martabarbera@users.noreply.github.com>
2021-05-14 17:00:14 +0100

    Update repeat.h

--- repeat.h
+++ repeat.h
@@ -5,7 +5,14 @@
 #include "program.h"
 
 //--------Teammember 2 to complete this section (class declaration) ----------
+class Repeat: public Command{
 
+public:
+	void run(int i);
+	friend std::istream& operator>>(std::istream& in, Program& prog);
+
+
+};
 //----------------------------------------------------------------------------
 
 #endif /* REPEAT_H_ */
