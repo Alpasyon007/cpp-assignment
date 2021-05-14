6cae88c5d561937edd9e11504439992c73c8a503
martabarbera <82161466+martabarbera@users.noreply.github.com>
2021-05-14 16:59:22 +0100

    Update repeat.cpp

--- repeat.cpp
+++ repeat.cpp
@@ -1,5 +1,28 @@
 #include "repeat.h"
 
 //--------Teammember 2 to complete this section (class definition) ----------
+friend std::istream& operator>>(std::istream& in, Program& prog){
+	int i;
+	char aux;
+	//Repeat rep;
+	in >> i;
+	in >>  aux;
+	
+	if (aux=="["){
+		in >> prog;
+		//rep.run(i); 
+	}
+	
+	
+}
+
+void run(int i){
+	int b;
+	Program pr;
+	for (b=0; b<i; b++){
+		pr.run;
+	}
+
+}
 
 //----------------------------------------------------------------------------
