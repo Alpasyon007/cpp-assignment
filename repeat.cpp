a07567fe7ae362ab9d4fbc1970ca4542a15e6d56
martabarbera <82161466+martabarbera@users.noreply.github.com>
2021-05-16 21:48:33 +0100

    Update repeat.cpp

--- repeat.cpp
+++ repeat.cpp
@@ -2,20 +2,27 @@
 
 //--------Teammember 2 to complete this section (class definition) ----------
 
+#include "repeat.h"
+
+//--------Teammember 2 to complete this section (class definition) ----------
+
+/*run funciton will call the program as many times as the repeat function has to do */
 void Repeat::run() {
-	for (int i(0); i < value; i++){
-		prog.run();
+	for (int i(0); i < value; i++){ //loop as many times as the value 
+		prog.run();//calls program 
 	}
 }
 
+/*The operator has access to the file and the point form which star reading*/
+
 std::istream& operator>>(std::istream& in, Repeat& rpt) {
-	std::string aux;
-	in >> rpt.value;
+	std::string aux; //to read file
+	in >> rpt.value;//save value to give to run function
 	in >> aux;
-	
-	if (aux == "[") {
+
+	if (aux == "[") {//check for the opening of brackets to give back to program 
 		in >> rpt.prog;
-	}	
+	}
 
 	return in;
 }
